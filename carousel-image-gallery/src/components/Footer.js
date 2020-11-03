2db39f899931b88451b5d5714d688f3fb051d62a
Caleb Duff <calebdduff@gmail.com>
2020-11-03 16:27:54 +0100

    Added Footer Component

---
+++ carousel-image-gallery/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div>
+            <p> Designed by Duffigoogle &copy 2020.</p>
+        </div>
+    )
+}
+
+export default Footer
