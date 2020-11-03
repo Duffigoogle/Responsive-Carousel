7ef3591678eb1f258e16fc3ba8f1df379b7511b4
Caleb Duff <calebdduff@gmail.com>
2020-11-03 11:11:58 +0100

    Changed header color

--- carousel-image-gallery/src/components/Header.js
+++ carousel-image-gallery/src/components/Header.js
@@ -6,28 +6,24 @@ import { FaSearch } from 'react-icons/fa';
 
 class Nav extends React.Component {
     render() {
-        return (
-            <Container>
-                <Navbar expand='lg' variant='light' bg='light'>
-                        <Navbar.Brand href='#'><h2><u>Trippa</u><span>Shots</span></h2></Navbar.Brand>
+        return ( 
+                <Navbar expand='lg' variant='light' bg='dark' fixed="top">
+                    <Container>
+                        <Navbar.Brand href='#'><h5><u>Trippa</u><span>Shots</span></h5></Navbar.Brand>
                         <Form inline>
                             <FormControl type="text" placeholder="Search" className=" mr-sm-2" />
                             <Button type="submit"> <FaSearch /> </Button>
                         </Form>
+                    </Container> 
                 </Navbar>
-                {/* Add social media icons */}
-                {/* FcSearch */}
-                
-            </Container>    
         );
     };
 }
 
 function Header() {
     return (
-        <div>
+        <div className='header'>
             <Nav />
-            <h1>TrippaShots</h1>
         </div>
     );
 }
